abf405a9e7096fde69815c506f3b9dcd7c385548
Daniil <daniiltitov538@gmail.com>
2024-10-11 22:02:55 +0300

    Программа проходит все тесты

--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -1,10 +1,26 @@
-#include <assert.h>
-#include "str2int.h"
-#include "stdio.h"
+#include <stdio.h>
 
 int str2int(const char *str) {
-
-
+    int a = 0;
+    int b = 1;
     
-    return 0;
+    // Пропускаем пробелы
+    while (*str == ' ') {
+        str++;
+    }
+    // Проверка на знак
+    if (*str == '-') {
+        b = -1;
+        str++;
+    } else if (*str == '+') {
+        str++;
+    }
+    // Преобразование символов в число
+    while (*str >= '0' && *str <= '9') {
+        a = a * 10 + (*str - '0');
+        str++;
+    }
+
+    return b * a;
 }
+    return 0;
