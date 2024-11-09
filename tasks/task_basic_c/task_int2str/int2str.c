5854d27adba86e4251b2d0bfe3fcdc15eb0c4b10
Daniil <daniiltitov538@gmail.com>
2024-11-09 10:38:30 +0300

    Программа проходит все тесты

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -3,36 +3,39 @@
 
 char* int2str(int num) {
     int size = 0;
-    int znak = 0;
+    int symbol = 0;
 
-    long long number = num;
-    
-    if (number<0){
-        number = -number;
+    if (num<0){
+        num = -num;
         size++;
-        znak = 1;
+        symbol = 1;
     }
-    long long v = number;
+
+    unsigned int number = num;
+    
+    unsigned int  v = number;
+
     while (v>0){
         size++;
         v/=10;
     }
-    
-    char* stroka = malloc((size+1)*(sizeof(char)));
 
-    if (number == 0 ){
+    char * str = malloc((size+1)*(sizeof(char)));
+
+    if (number == 0){
         return "0";
     }
 
-    if (znak==1){
-        stroka[0]='-';
+    if (symbol == 1){
+        str[0]='-';
     }
-    
-    stroka[size] = '\0';
 
-    for (int i=size-1; i>=znak; i--){
-        stroka[i]=(number%10)+'0';
+    str[size] = '\0';
+
+    for (int i=size-1; i>=symbol; i--){
+        str[i]=(number%10)+'0';
         number/=10;
     }
-    return stroka;
-    }
+
+    return str;
+}
