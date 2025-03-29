82540df6eecc16a039672fcd1ba3c74b72a8e636
Daniil <daniiltitov538@gmail.com>
2025-03-29 15:31:59 +0300

    strint and basic_c

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,9 +1,59 @@
+// Copyright 2025 Samoylov Daniil
 #include "int2str.h"
-#include <assert.h>
-#include "stdio.h"
+#include <stdlib.h>
 
-char* int2str(int number)
+char* int2str(int num)
 {
-	char* str = "0";
+	int size = 0;
+	int symbol = 0;
+
+	if (num < 0)
+	{
+		num = -num;
+		size++;
+		symbol = 1;
+	}
+
+	unsigned int number = num;
+	unsigned int v = number;
+
+	if (number == 0)
+	{
+		size = 1;
+	}
+	else
+	{
+		while (v > 0)
+		{
+			size++;
+			v /= 10;
+		}
+	}
+
+	char* str = malloc((size + 1) * sizeof(char));
+	if (str == NULL)
+	{
+		return NULL;
+	}
+
+	if (number == 0)
+	{
+		str[0] = '0';
+		str[1] = '\0';
+		return str;
+	}
+
+	if (symbol == 1)
+	{
+		str[0] = '-';
+	}
+
+	str[size] = '\0';
+
+	for (int i = size - 1; i >= symbol; i--)
+	{
+		str[i] = (number % 10) + '0';
+		number /= 10;
+	}
 	return str;
 }
